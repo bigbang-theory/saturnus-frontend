bc55a9b6aa1ca6c577bb31a1bdf74737922d5d8b
23Bas <agung.ngurah145@gmail.com>
2023-04-07 13:08:02 +0700

    Membuat tampilan Register

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("test")
+from flask import Flask, render_template, request, \
+      session, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('register2.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
